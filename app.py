cfb4c45ec531da057fc2d1a7f3e7807e5d195f7e
himanshukumar30 <himanshuk.kumar01@gmail.com>
2023-02-02 17:01:51 -0800

    adding Cupcake model

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
 """Flask app for Cupcakes"""
+from flask import Flask, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+app.app_context().push() 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///employees_db'
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = "abc123"
+
+connect_db(app)
+toolbar = DebugToolbarExtension(app)
--- models.py
+++ models.py
@@ -1,1 +1,23 @@
 """Models for Cupcake app."""
+from flask_sqlalchemy import SQLAlchemy
+db=SQLAlchemy()
+
+def connect_db(app):
+    """Connect the database to our Flask app."""
+
+    db.app = app
+    db.init_app(app)
+
+default_image = 'https://tinyurl.com/y63x78zs'
+
+
+class Cupcake(db.Model):
+    '''Cupcakes model to structure cupcake details'''
+    __tablename__='cupcakes'
+    
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    flavor = db.Column(db.Text, nullable = False)
+    size = db.Column(db.Text, nullable = False)
+    rating = db.Column(db.Float, nullable = False)
+    image = db.Column(db.Text, nullable = False, default = default_image)
+    
